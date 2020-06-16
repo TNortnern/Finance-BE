4b13baf80deb8fe7849af6ab80c7e4321fdd446c
TNORTHERN <traynorthern@yahoo.com>
2020-06-16 18:56:35 -0500

    mongoose replace

--- config/database.js
+++ config/database.js
@@ -1,15 +1,38 @@
+if (process.env.NODE_ENV === "production") {
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "mongoose",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "mongo",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
       },
       options: {
-        useNullAsDefault: true,
+        authenticationDatabase: env("AUTHENTICATION_DATABASE"),
+        ssl: true,
       },
     },
   },
 });
+} else {
+  module.exports = ({ env }) => ({
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  });
+}
