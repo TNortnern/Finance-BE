1963ef3f85227a426c5aa87fd425593229e90ef8
TNORTHERN <traynorthern@yahoo.com>
2020-06-25 12:32:48 -0500

    authentication

---
+++ api/user/config/schema.graphql.js
@@ -0,0 +1,27 @@
+module.exports = {
+  definition: `
+  type User {
+    name: String
+  }
+  type UserPayload {
+      jwt: String
+      user: UsersPermissionsUser
+  }
+  `,
+  mutation: `
+    registerUser(email: String, name: String, username: String, provider: String, password: String, company_name: String, company_email: String): UserPayload,
+    loginUser(identifier: String, password: String, provider: String): UserPayload!
+
+  `,
+  resolver: {
+    Mutation: {
+      registerUser: {
+        description: "Alternative to register user for this application",
+        resolver: "application::user.user.registerUser",
+      },
+      loginUser: {
+        resolver: "plugins::users-permissions.Auth.callback",
+      },
+    },
+  },
+};
