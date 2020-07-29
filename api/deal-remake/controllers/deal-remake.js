760294fc2f53d19318af1ce1aa8dd5298fc35202
TNORTHERN <traynorthern@yahoo.com>
2020-07-29 10:22:29 -0500

    not allowing deals to be approved without consent

--- api/deal-remake/controllers/deal-remake.js
+++ api/deal-remake/controllers/deal-remake.js
@@ -11,40 +11,43 @@ const rankingArrayHandler = async (ranking, key, value) => {
   // console.log('ranking', ranking)
   let arr = [];
   for await (const rank of value) {
-    let ranking_item = await strapi
-      .query("ranking-item")
-      .findOne({ value: rank.value });
-    if (
-      !ranking_item ||
-      ranking_item.ranking.name.toLowerCase() !== ranking.name.toLowerCase()
-    ) {
-      ranking_item = await strapi.query("ranking-item").create({
-        ranking: ranking.id,
-        value: rank.value,
-        deal: deal.id,
-        size_total: deal.Size.item.value,
-        dealAmount: 1,
-        deal_remakes: [deal.id],
-      });
-    } else {
-      const aritems = ranking_item.deal_remakes || [];
-      aritems.push(deal.id);
-      await strapi.query("ranking-item").update(
-        { id: ranking_item.id },
-        {
-          size_total: ranking_item.size_total + Number(deal.Size.item.value),
-          dealAmount: ranking_item.dealAmount + 1,
-          deal_remakes: aritems,
-        }
-      );
+    let ranking_item = rank;
+    if (deal.approved) {
+      let ranking_item = await strapi
+        .query("ranking-item")
+        .findOne({ value: rank.value });
+      if (
+        !ranking_item ||
+        ranking_item.ranking.name.toLowerCase() !== ranking.name.toLowerCase()
+      ) {
+        ranking_item = await strapi.query("ranking-item").create({
+          ranking: ranking.id,
+          value: rank.value,
+          deal: deal.id,
+          size_total: deal.Size.item.value,
+          dealAmount: 1,
+          deal_remakes: [deal.id],
+        });
+      } else {
+        const aritems = ranking_item.deal_remakes || [];
+        aritems.push(deal.id);
+        await strapi.query("ranking-item").update(
+          { id: ranking_item.id },
+          {
+            size_total: ranking_item.size_total + Number(deal.Size.item.value),
+            dealAmount: ranking_item.dealAmount + 1,
+            deal_remakes: aritems,
+          }
+        );
+      }
     }
     arr = [
       ...arr,
       {
         item: {
-          value: ranking_item.value,
-          status: rank.status || null,
-          id: ranking_item.id,
+          value: ranking_item.value || null,
+          status: ranking_item.status || null,
+          id: ranking_item.id || null,
         },
       },
     ];
@@ -59,25 +62,28 @@ const rankingArrayHandler = async (ranking, key, value) => {
 const filterArrayHandler = async (filter, key, value) => {
   let arr = [];
   for await (const filt of value) {
-    let filter_item = await strapi
-      .query("filter-item")
-      .findOne({ value: filt.value });
-    if (
-      !filter_item ||
-      filter_item.filter.name.toLowerCase() !== filter.name.toLowerCase()
-    ) {
-      filter_item = await strapi.query("filter-item").create({
-        filter: filter.id,
-        value: filt,
-      });
+    let filter_item = filt;
+    if (deal.approved) {
+      filter_item = await strapi
+        .query("filter-item")
+        .findOne({ value: filt.value });
+      if (
+        !filter_item ||
+        filter_item.filter.name.toLowerCase() !== filter.name.toLowerCase()
+      ) {
+        filter_item = await strapi.query("filter-item").create({
+          filter: filter.id,
+          value: filt,
+        });
+      }
     }
     arr = [
       ...arr,
       {
         item: {
-          value: filter_item.value,
+          value: filter_item.value || null,
           status: filter.status || null,
-          id: filter_item.id,
+          id: filter_item.id || null,
         },
       },
     ];
@@ -96,31 +102,33 @@ const filterItemHandler = async (filter, key, item) => {
     await filterArrayHandler(filter, key, item.value);
     return;
   }
-  let filter_item = await strapi
-    .query("filter-item")
-    .findOne({ value: item.value });
-  if (
-    !filter_item ||
-    !filter ||
-    filter_item.filter.name.toLowerCase() !== filter.name.toLowerCase()
-  ) {
-    filter_item = await strapi.query("filter-item").create({
-      filter: filter.id,
-      value: item.value,
-    });
+  let filter_item = item;
+  if (deal.approved) {
+    filter_item = await strapi
+      .query("filter-item")
+      .findOne({ value: item.value });
+    if (
+      !filter_item ||
+      !filter ||
+      filter_item.filter.name.toLowerCase() !== filter.name.toLowerCase()
+    ) {
+      filter_item = await strapi.query("filter-item").create({
+        filter: filter.id,
+        value: item.value,
+      });
+    }
+    if (!filter_item) {
+      return;
+    }
   }
-  if (!filter_item) {
-    return;
-  }
-  // console.log('gets this far', value)
   await strapi.query("deal-remake").update(
     { id: deal.id },
     {
       [key]: {
         item: {
-          value: filter_item.value,
+          value: filter_item.value || null,
           status: item.status || null,
-          id: filter_item.id,
+          id: filter_item.id || null,
         },
       },
     }
@@ -134,11 +142,6 @@ module.exports = {
   },
   async filterHandler(filter, key, item, passedDeal) {
     deal = passedDeal;
-    // if (key === "Is_EBITDA_above_10m") {
-    //   return await strapi
-    //     .query("deal-remake")
-    //     .update({ id: deal.id }, { [key]: { value, status: null } });
-    // }
     await filterItemHandler(filter, key, item);
   },
   async rankingHandler(ranking, key, item, passedDeal) {
@@ -149,51 +152,51 @@ module.exports = {
       await rankingArrayHandler(ranking, key, item.value);
       return;
     }
-    let ranking_item = await strapi
-      .query("ranking-item")
-      .findOne({ value: item.value });
-    let resolvedValue = ranking_item ? ranking_item.value : "";
-    let resolvedId = ranking_item ? ranking_item.id : "";
-    let matcher;
-    if (ranking_item) {
-      if (key.includes("_")) {
-        let temp = key.split("_")[0] + " " + key.split("_")[1];
-        matcher =
-          temp.toLowerCase() === ranking_item.ranking.name.toLowerCase();
-      } else {
-        matcher = key.toLowerCase() === ranking_item.ranking.name.toLowerCase();
-      }
-    }
-    if (!matcher) {
-      ranking_item = await strapi.query("ranking-item").create({
-        ranking: ranking.id,
-        value: item.value,
-        size_total: deal.Size.item.value,
-        dealAmount: 1,
-        deal_remakes: [deal.id],
-      });
-      resolvedValue = ranking_item.value;
-      resolvedId = ranking_item.id;
-    } else {
-      const aritems = ranking_item.deal_remakes || [];
-      aritems.push(deal.id);
-      await strapi.query("ranking-item").update(
-        { id: ranking_item.id },
-        {
-          size_total: ranking_item.size_total + Number(deal.Size.item.value),
-          dealAmount: ranking_item.dealAmount + 1,
-          deal_remakes: aritems
+    let ranking_item = item;
+    if (deal.approved) {
+      ranking_item = await strapi
+        .query("ranking-item")
+        .findOne({ value: item.value });
+      let matcher;
+      if (ranking_item) {
+        if (key.includes("_")) {
+          let temp = key.split("_")[0] + " " + key.split("_")[1];
+          matcher =
+            temp.toLowerCase() === ranking_item.ranking.name.toLowerCase();
+        } else {
+          matcher =
+            key.toLowerCase() === ranking_item.ranking.name.toLowerCase();
         }
-      );
+      }
+      if (!matcher) {
+        ranking_item = await strapi.query("ranking-item").create({
+          ranking: ranking.id,
+          value: item.value,
+          size_total: deal.Size.item.value,
+          dealAmount: 1,
+          deal_remakes: [deal.id],
+        });
+      } else {
+        const aritems = ranking_item.deal_remakes || [];
+        aritems.push(deal.id);
+        await strapi.query("ranking-item").update(
+          { id: ranking_item.id },
+          {
+            size_total: ranking_item.size_total + Number(deal.Size.item.value),
+            dealAmount: ranking_item.dealAmount + 1,
+            deal_remakes: aritems,
+          }
+        );
+      }
     }
     return await strapi.query("deal-remake").update(
       { id: deal.id },
       {
         [capitalize(key)]: {
           item: {
-            value: resolvedValue || value,
-            status: item.status || null,
-            id: resolvedId,
+            value: ranking_item.value || null,
+            status: ranking_item.status || null,
+            id: ranking_item.id || null,
             isRanking: true,
           },
         },
--- api/deal-remake/services/deal-remake.js
+++ api/deal-remake/services/deal-remake.js
@@ -25,11 +25,82 @@ const resolveRanking = async (ranking, key, value) => {
   );
 };
 
+const handleDeal = async (dealData) => {
+  const rankings = [
+    {
+      id: "5ef782a0eaa67c240465a46d",
+      name: "Lender",
+    },
+    {
+      id: "5ef782a8eaa67c240465a46e",
+      name: "Debt advisor",
+    },
+    {
+      id: "5ef782aceaa67c240465a46f",
+      name: "Sponsor",
+    },
+    {
+      id: "5ef782b4eaa67c240465a470",
+      name: "Sponsor counsel",
+    },
+    {
+      id: "5ef783a9eaa67c240465a471",
+      name: "Lender counsel",
+    },
+  ];
+  const filters = await strapi.query("filter").find({});
+  const isRanking = (item) => rankings.find((rank) => rank.name === item);
+  for (let x in dealData) {
+    let filter = null;
+    let ranking = null;
+    let current = dealData[x];
+    x = capitalize(x);
+    // size and comments already have been extracted so just ignore those fields
+    if (x !== "Comments" && x !== "Size") {
+      // check if the object key contains a underscore, if so we need to handle these differently to find filters/rankings
+      if (x.includes("_")) {
+        const splitup = x.split("_");
+        const reprinted = `${splitup[0]} ${splitup[1]}`;
+        ranking = isRanking(reprinted);
+        if (ranking) {
+          console.log("ranking1", ranking, dealData[x]);
+          resolveRanking(ranking, x, current);
+        } else {
+          filter = filters.find((filt) => filt.name === reprinted);
+          if (filter) {
+            resolveFilter(filter, x, current);
+          }
+        }
+      } else {
+        ranking = isRanking(x);
+        if (ranking) {
+          console.log("ranking1", ranking, dealData[x]);
+          resolveRanking(ranking, x, current);
+        } else {
+          let specialCase = "";
+          if (specialCase) {
+            filter = filters.find(
+              (filt) => filt.name.toLowerCase() === specialCase.toLowerCase()
+            );
+          } else {
+            filter = filters.find(
+              (filt) => filt.name.toLowerCase() === x.toLowerCase()
+            );
+          }
+          if (filter) {
+            resolveFilter(filter, x, current);
+          }
+        }
+      }
+    }
+  }
+};
+
 module.exports = {
   baseCreateDealRemake: async (dealData, title, approved, author) => {
     const getUser = await strapi
       .query("user", "users-permissions")
-      .findOne({ id: author });
+      .findOne({ _id: author });
     // console.log('getUser', getUser)
     // if submitted deal wasn't from administrator then set the approved value to false.
     if (!getUser || !author) {
@@ -40,7 +111,6 @@ module.exports = {
       }
     }
     // console.log("dealData", dealData);
-    const filters = await strapi.query("filter").find({});
     let comments = (dealData.Comments && dealData.Comments.value) || "";
     deal = await strapi.query("deal-remake").create({
       title,
@@ -58,7 +128,7 @@ module.exports = {
         item: {
           value:
             (dealData.Year && dealData.Year.value) ||
-            `20${dealData.Month.split(" ")[1]}` ||
+            `20${dealData.Month.value.split(" ")[1]}` ||
             null,
           status: (dealData.Year && dealData.Year.status) || null,
           id: null,
@@ -74,76 +144,15 @@ module.exports = {
     });
     if (!deal.Year.item.value) {
       await strapi.query("deal-remake").delete({ id: deal.id });
-      console.log('deal deleted', deal.id)
+      console.log("deal deleted", deal.id);
       return;
     }
-    const rankings = [
-      {
-        id: "5ef782a0eaa67c240465a46d",
-        name: "Lender",
-      },
-      {
-        id: "5ef782a8eaa67c240465a46e",
-        name: "Debt advisor",
-      },
-      {
-        id: "5ef782aceaa67c240465a46f",
-        name: "Sponsor",
-      },
-      {
-        id: "5ef782b4eaa67c240465a470",
-        name: "Sponsor counsel",
-      },
-      {
-        id: "5ef783a9eaa67c240465a471",
-        name: "Lender counsel",
-      },
-    ];
-    const isRanking = (item) => rankings.find((rank) => rank.name === item);
-    for (let x in dealData) {
-      let filter = null;
-      let ranking = null;
-      let current = dealData[x];
-      x = capitalize(x);
-      // size and comments already have been extracted so just ignore those fields
-      if (x !== "Comments" && x !== "Size") {
-        // check if the object key contains a underscore, if so we need to handle these differently to find filters/rankings
-        if (x.includes("_")) {
-          const splitup = x.split("_");
-          const reprinted = `${splitup[0]} ${splitup[1]}`;
-          ranking = isRanking(reprinted);
-          if (ranking) {
-            console.log("ranking1", ranking, dealData[x]);
-            resolveRanking(ranking, x, current);
-          } else {
-            filter = filters.find((filt) => filt.name === reprinted);
-            if (filter) {
-              resolveFilter(filter, x, current);
-            }
-          }
-        } else {
-          ranking = isRanking(x);
-          if (ranking) {
-            console.log("ranking1", ranking, dealData[x]);
-            resolveRanking(ranking, x, current);
-          } else {
-            let specialCase = "";
-            if (specialCase) {
-              filter = filters.find(
-                (filt) => filt.name.toLowerCase() === specialCase.toLowerCase()
-              );
-            } else {
-              filter = filters.find(
-                (filt) => filt.name.toLowerCase() === x.toLowerCase()
-              );
-            }
-            if (filter) {
-              resolveFilter(filter, x, current);
-            }
-          }
-        }
-      }
-    }
+    await handleDeal(dealData);
     return deal;
   },
+  baseUpdateDealRemake: async (dealData) => {
+    if (dealData && dealData.approved) {
+      await handleDeal(dealData);
+    }  
+  }
 };
