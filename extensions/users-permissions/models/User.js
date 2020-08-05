22af8e73e4d81dbca9b1235c9ade915c5150b3cf
TNORTHERN <traynorthern@yahoo.com>
2020-08-05 18:54:21 -0500

    adding swagger, email confirmation on approval

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -6,15 +6,34 @@
 module.exports = {
   lifecycles: {
     async afterUpdate(result, params, data) {
+      const axios = require("axios");
       const user = await strapi
         .query("user", "users-permissions")
         .findOne({ id: params._id });
-      if (data.approved && data.views_left === 0) {
-        const defaults = await strapi.query("defaults").findOne({});
-        console.log("defaults", defaults);
-        await strapi
-          .query("user", "users-permissions")
-          .update({ id: params._id }, { views_left: defaults.view_count });
+      if (data.approved) {
+        if (data.views_left === 0) {
+          const defaults = await strapi.query("defaults").findOne({});
+
+          console.log("defaults", defaults);
+          await strapi
+            .query("user", "users-permissions")
+            .update({ id: params._id }, { views_left: defaults.view_count });
+        }
+        if (!data.confirmed) {
+          await axios
+            .post(
+              `https://finance-strapi-cms.herokuapp.com/auth/send-email-confirmation`,
+              {
+                email: user.email, // user's email
+              }
+            )
+            .then((response) => {
+              console.log("Your user received an email");
+            })
+            .catch((error) => {
+              console.error("An error occurred:");
+            });
+        }
       }
     },
   },
