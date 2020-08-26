4a1b9ecc4c979eb68f7a8cf3ee85851378658a27
TNORTHERN <traynorthern@yahoo.com>
2020-08-26 05:15:01 -0500

    transforming pagination to traditional pagination

--- api/deal-remake/config/schema.graphql.js
+++ api/deal-remake/config/schema.graphql.js
@@ -4,10 +4,9 @@ module.exports = {
   definition: `
     type DealRemakePayload {
       deals: [DealRemake],
-      nextCursor: String,
-      prevCursor: String,
       hasMore: Boolean,
-      total: Int
+      total: Int,
+      totalPages: Int
     }
   `,
   mutation: `
@@ -16,7 +15,7 @@ module.exports = {
 
   `,
   query: `
-    dealFilter(where: JSON, sort: String, limit: Int, cursor: String ): DealRemakePayload
+    dealFilter(where: JSON, sort: String, limit: Int, page: Int ): DealRemakePayload
   `,
   resolver: {
     Query: {
@@ -28,13 +27,13 @@ module.exports = {
         resolver: async (parent, data, { context }) => {
           const { query } = context;
           const limit = query._limit;
-          let cursor = query._cursor || "";
+          const page = query._page - 1;
           let hasMore = true;
           const sort = query._sort ? query._sort : "-Month.item.value";
           // removing these so that the query doesn't use them, instead they'll be used in custom ways
           delete query._limit;
           delete query._sort;
-          delete query._cursor;
+          delete query._page;
           const queryToArray = Object.keys(query);
           queryToArray.forEach((item, i) => {
             if (item.includes("Approved")) {
@@ -47,70 +46,22 @@ module.exports = {
               query[item] = { $in: query[item] };
             }
           });
-          // console.log('query', query)
-          const sortDesc = sort.charAt(0) === "-";
-          const whereDisplay = cursor
-            ? "Month.item.value"
-            : sortDesc
-            ? sort.substr(1)
-            : sort;
-          let oppositeSort = "";
-          let lastItem = null;
-          const handleOrder = () => {
-            if (!cursor) {
-              return;
-            } else {
-              if (sortDesc) {
-                oppositeSort = "Month.item.value";
-                return "lt";
-              }
-              oppositeSort = "-Month.item.value";
-              return "gt";
-            }
-          };
-          let deals;
-          if (!cursor) {
-           
-            deals = await strapi
-              .query("deal-remake")
-              .model.find(query)
-              .sort(sort)
-              .limit(Number(limit));
-            lastItem = await strapi
-              .query("deal-remake")
-              .model.findOne(query)
-              .sort(oppositeSort)
-              .limit(1)
-          } else {
-            deals = await strapi
-              .query("deal-remake")
-              .model.find(query)
-              .sort(sort)
-              .where(whereDisplay)
-              [handleOrder()](cursor)
-              .limit(Number(limit));
-            lastItem = await strapi
-              .query("deal-remake")
-              .model.findOne(query)
-              .sort(oppositeSort)
-          }
+          const deals = await strapi
+            .query("deal-remake")
+            .model.find(query)
+            .sort(sort)
+            .skip(page * Number(limit))
+            .limit(Number(limit));
           const total = await strapi
             .query("deal-remake")
             .model.countDocuments(query);
-          nextCursor = (deals.length && deals[deals.length - 1].Month.item.value) || "";
-          prevCursor = (deals.length && deals[0].Month.item.value) || "";
-          // console.log('lastItem', lastItem)
-          // console.log('lastItem._id', lastItem._id)
-          // console.log("object", deals.map(it => it._id));
-          // console.log(
-          //   "foundit?",
-          //   deals.find((ea) => ea._id.toString() == lastItem._id.toString())
-          // );
-          if (deals.find((each) => each._id.toString() === lastItem._id.toString())) {
+          const totalPages = Math.ceil(total / limit);
+          if (page + 1 !== totalPages) {
+            hasMore = true;
+          } else {
             hasMore = false;
-            nextCursor = "";
           }
-          return { deals, prevCursor, nextCursor, hasMore, total };
+          return { deals, hasMore, total, totalPages };
         },
       },
     },
